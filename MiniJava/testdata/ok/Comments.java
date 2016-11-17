12f5dabda40b419eb505e08f01c67ca5c1900592
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2016-11-17 07:54:22 +0100

    Add ok and error test cases.
    Check valid and invalid cases of comments use.

---
+++ MiniJava/testdata/ok/Comments.java
@@ -0,0 +1,14 @@
+/*
+ * This is a normal multiline comment.
+ */
+
+// This is a oneliner
+class A {
+	/**
+	 * This is a doc block comment. Any char is valid, even that ¥
+	 * @args no args
+	 */
+	public static void main(String[] args){
+		
+	}
+}
