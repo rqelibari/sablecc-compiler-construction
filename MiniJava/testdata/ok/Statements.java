2dff53b948c6ad8e0b965e753bed65df9c729601
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2016-11-17 11:08:22 +0100

    Add ok test case.
    Check for different type of statements.

---
+++ MiniJava/testdata/ok/Statements.java
@@ -0,0 +1,17 @@
+class A {
+	public static void main(String[] args){
+		int a;
+		if (a > 5) System.out.println(Exp);
+		b = a;
+		a[5] = a;
+		while(a) i = 4;
+	}
+}
+
+class B {
+	public int myMethod(int a, boolean b) {
+		a[5] = a;
+		while(a) i = 4;
+		return true;
+	}
+}
